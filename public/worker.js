82aa66e4f760af17b0a12e85d451d5e4656afaaf
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2022-12-26 16:55:30 +0500

    pwa crated

---
+++ public/worker.js
@@ -0,0 +1,20 @@
+let CACHE_NAME = "PWA APP";
+let filesToCache = ["index.html", "offline.html"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(filesToCache)
+        })
+    )
+})
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+            .catch(() => caches.match("offline.html"))
+        })
+    )
+})
