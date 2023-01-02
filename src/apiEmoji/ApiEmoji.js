2f57ff48b52d25acca2fa0048cff9e67d2734b69
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-02 14:57:19 +0500

    Emoji's Changed

--- src/apiEmoji/ApiEmoji.js
+++ src/apiEmoji/ApiEmoji.js
@@ -1,35 +1,19 @@
-// import axios from 'axios'
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-const ACCESS_KEY = "e4e8c1eddab35e74a281f6506f4437beea763f7b"
+import emoji from 'emoji-api'
 
 function ApiEmoji() {
-    // const [query, setQuery] = useState("")
-    const [result, setResult] = useState({})
-    console.log(result);
-    const search = async () => {
-        const response = await fetch(`https://emoji-api.com/emojis?access_key=${ACCESS_KEY}`);
-        const data = await response.json()
-        setResult(data)
-        console.log(data);
-    }
-    useEffect(() => {
-        search()
-    }, [])
+    const emoji = require("emoji-api");
+    const setSmile = emoji.all()
+    console.log(setSmile)
     return (
-        <div>
-            {/* <input type="text" className='search' placeholder='Search...' onChange={e => setQuery(e.target.value)} /> */}
-            <button onClick={search}>Search</button>
-            <ul className="list">
-                {result.map((emoji, id) => {
-                    return (
-                        <div key={id} className="box">
-                            <h1>{emoji.character}</h1>
-                        </div>
-                    )
-                })}
-            </ul>
+        <div className='list'>
+            {setSmile.map((emoji, id) => {
+                return (
+                    <div key={id} className="box">
+                        <h1>{emoji._data.emoji}</h1>
+                    </div>
+                )
+            })}
         </div>
     )
 }
