997040cf8bf95d23c79c6c635a4c6c4a561741ad
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-03 17:30:56 +0500

    Toast position changed

--- src/apiEmoji/Open.js
+++ src/apiEmoji/Open.js
@@ -5,35 +5,33 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Open({ newEmoji, close }) {
-  
-  const copy = () => {
-    navigator.clipboard.writeText(newEmoji.emoji)
-    toast.success(`${newEmoji.emoji} Copied To Clipboard!`, {
-      draggablePercent: 60
-    });
-  }
 
-  return (
-    <div className='card'>
-      <h1>{newEmoji.codepoints}</h1>
-      <h1>{newEmoji.emoji}</h1>
-      <h1>{newEmoji.name}</h1>
-      <h1>{newEmoji.group}</h1>
-      <h1>{newEmoji.sub_group}</h1>
-      <HiOutlineClipboardCopy onClick={() => copy()} />
-      <FaTimes onClick={close} />
-      <ToastContainer position="bottom-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light" />
-    </div>
-  )
+    const copy = () => {
+        navigator.clipboard.writeText(newEmoji.emoji)
+        toast.success(`${newEmoji.emoji} Copied To Clipboard!`);
+    }
+
+    return (
+        <div className='card'>
+            <h1>{newEmoji.codepoints}</h1>
+            <h1>{newEmoji.emoji}</h1>
+            <h1>{newEmoji.name}</h1>
+            <h1>{newEmoji.group}</h1>
+            <h1>{newEmoji.sub_group}</h1>
+            <HiOutlineClipboardCopy onClick={() => copy()} />
+            <FaTimes onClick={close} />
+            <ToastContainer position="bottom-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light" />
+        </div>
+    )
 }
 
 export default Open
