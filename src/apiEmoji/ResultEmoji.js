31172b370d6ee8ac4359a6a8896fa8691a97466e
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-04 15:10:37 +0500

    Emoji not found

--- src/apiEmoji/ResultEmoji.js
+++ src/apiEmoji/ResultEmoji.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Open from "./Open"
 import { HiOutlineChevronUp } from "react-icons/hi"
+import emojiNotFound from "../noInternet.gif";
 
 function ResultEmoji({ result }) {
 
@@ -38,13 +39,18 @@ function ResultEmoji({ result }) {
     return (
         <>
             <div className='list'>
-                {result.map((emoji) => {
-                    return (
-                        <div key={emoji._data.codepoints} className='box' onClick={() => openEmoji(emoji._data)}>
-                            <h1>{emoji._data.emoji}</h1>
-                        </div>
-                    )
-                })}
+                {result.length > 0 ? <>
+                    {result.map((emoji) => {
+                        return (
+                            <div key={emoji._data.codepoints} className='box' onClick={() => openEmoji(emoji._data)}>
+                                <h1>{emoji._data.emoji}</h1>
+                            </div>
+                        )
+                    })}
+                </> : <div className='notFound'>
+                    <img src={emojiNotFound} alt="Emoji Not found!" />
+                    <p>Emojies not found!</p>
+                </div>}
             </div>
             {isOpen ? <Open newEmoji={newEmoji} close={() => setIsOpen(false)} /> : ""}
             {scrollBtn ? <HiOutlineChevronUp className='up' onClick={scrollToTop} /> : ""}
